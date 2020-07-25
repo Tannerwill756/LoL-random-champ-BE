f9391f501c1261099d6ad769479fd5dc69c2afcc
Tanner Williams <tannerwill756@gmail.com>
2020-07-25 18:52:24 -0400

    endpoints only send 1 random champ per request

--- backend/requests/request.js
+++ backend/requests/request.js
@@ -1,66 +1,72 @@
-const express = require('express');
+const express = require("express");
 
-const Champs = require('./request-model.js');
+const Champs = require("./request-model.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    Champs.find()
-    .then(champions => {
-        res.json(champions);
+router.get("/", (req, res) => {
+  Champs.find()
+    .then((champions) => {
+      res.json(champions);
     })
-    .catch(err => {
-        res.status(500).json({ message: 'Failed to get champions' });
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to get champions" });
     });
 });
 
-router.get('/top', (req, res) => {
-    Champs.findTop()
-    .then(champions => {
-        res.json(champions);
+router.get("/top", (req, res) => {
+  Champs.findTop()
+    .then((champions) => {
+      var randChamp = Math.floor(Math.random() * champions.length);
+      res.json(champions[randChamp]);
     })
-    .catch(err => {
-        res.status(500).json({ message: 'Failed to get champions' });
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to get champions" });
     });
 });
 
-router.get('/jungle', (req, res) => {
-    Champs.findJng()
-    .then(champions => {
-        res.json(champions);
+router.get("/jungle", (req, res) => {
+  Champs.findJng()
+    .then((champions) => {
+      var randChamp = Math.floor(Math.random() * champions.length);
+      res.json(champions[randChamp]);
     })
-    .catch(err => {
-        res.status(500).json({ message: 'Failed to get champions' });
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to get champions" });
     });
 });
 
-router.get('/mid', (req, res) => {
-    Champs.findMid()
-    .then(champions => {
-        res.json(champions);
+router.get("/mid", (req, res) => {
+  Champs.findMid()
+    .then((champions) => {
+      var randChamp = Math.floor(Math.random() * champions.length);
+      res.json(champions[randChamp]);
     })
-    .catch(err => {
-        res.status(500).json({ message: 'Failed to get champions' });
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to get champions" });
     });
 });
 
-router.get('/adc', (req, res) => {
-    Champs.findAdc()
-    .then(champions => {
-        res.json(champions);
+router.get("/adc", (req, res) => {
+  Champs.findAdc()
+    .then((champions) => {
+      var randChamp = Math.floor(Math.random() * champions.length);
+      res.json(champions[randChamp]);
     })
-    .catch(err => {
-        res.status(500).json({ message: 'Failed to get champions' });
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to get champions" });
     });
 });
 
-router.get('/sup', (req, res) => {
-    Champs.findSup()
-    .then(champions => {
-        res.json(champions);
+router.get("/sup", (req, res) => {
+  Champs.findSup()
+    .then((champions) => {
+      var randChamp = Math.floor(Math.random() * champions.length);
+      res.json(champions[randChamp]);
     })
-    .catch(err => {
-        res.status(500).json({ message: 'Failed to get champions' });
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to get champions" });
     });
 });
+
 module.exports = router;
