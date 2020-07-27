6527c88a1c6ea076230a2911a095230cccb9c86d
Tanner Williams <tannerwill756@gmail.com>
2020-07-27 15:28:47 -0400

    added a get team endpoint

--- backend/requests/request-model.js
+++ backend/requests/request-model.js
@@ -1,34 +1,34 @@
 const db = require("../data/db-config.js");
 
 module.exports = {
-    find,
-    findTop,
-    findJng,
-    findMid,
-    findAdc,
-    findSup
-}
+  find,
+  findTop,
+  findJng,
+  findMid,
+  findAdc,
+  findSup,
+};
 
 function find() {
-    return db.select('*').from('champions');
+  return db.select("*").from("champions");
 }
 
 function findTop() {
-    return db.select('*').from('champions').where('top', '>', '0')
+  return db.select("*").from("champions").where("top", ">", "0");
 }
 
 function findJng() {
-    return db.select('*').from('champions').where('jungle', '>', '0')
+  return db.select("*").from("champions").where("jungle", ">", "0");
 }
 
 function findMid() {
-    return db.select('*').from('champions').where('mid', '>', '0')
+  return db.select("*").from("champions").where("mid", ">", "0");
 }
 
 function findAdc() {
-    return db.select('*').from('champions').where('adc', '>', '0')
+  return db.select("*").from("champions").where("adc", ">", "0");
 }
 
 function findSup() {
-    return db.select('*').from('champions').where('support', '>', '0')
+  return db.select("*").from("champions").where("support", ">", "0");
 }
--- backend/requests/request.js
+++ backend/requests/request.js
@@ -69,4 +69,56 @@ router.get("/sup", (req, res) => {
     });
 });
 
+router.get("/team", (req, res) => {
+  findTeam();
+  function findTeam() {
+    var newTeam = [];
+    Champs.findTop().then((champions) => {
+      var randChamp = Math.floor(Math.random() * champions.length);
+      var chosenChamp = champions[randChamp];
+      newTeam.push(chosenChamp);
+
+      Champs.findJng().then((champions) => {
+        var randChamp = Math.floor(Math.random() * champions.length);
+        var chosenChamp = champions[randChamp];
+        newTeam.push(chosenChamp);
+
+        Champs.findMid().then((champions) => {
+          var randChamp = Math.floor(Math.random() * champions.length);
+          var chosenChamp = champions[randChamp];
+          newTeam.push(chosenChamp);
+
+          Champs.findAdc().then((champions) => {
+            var randChamp = Math.floor(Math.random() * champions.length);
+            var chosenChamp = champions[randChamp];
+            newTeam.push(chosenChamp);
+
+            Champs.findSup().then((champions) => {
+              var randChamp = Math.floor(Math.random() * champions.length);
+              var chosenChamp = champions[randChamp];
+              newTeam.push(chosenChamp);
+
+              const idArray = [];
+
+              newTeam.map((obj) => {
+                idArray.push(obj.id);
+              });
+
+              var sorted_arr = idArray.slice().sort();
+
+              for (var i = 0; i < sorted_arr.length - 1; i++) {
+                if (sorted_arr[i + 1] === sorted_arr[i]) {
+                  return findTeam();
+                } else {
+                  return res.json(newTeam);
+                }
+              }
+            });
+          });
+        });
+      });
+    });
+  }
+});
+
 module.exports = router;
