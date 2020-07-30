4768c82c0c58230d051b0593f80363ac9f76695f
Tanner Williams <tannerwill756@gmail.com>
2020-07-30 13:38:43 -0400

    deploying

--- data/migrations/20200721104540_champ-table.js
+++ data/migrations/20200721104540_champ-table.js
@@ -1,20 +1,16 @@
-
-exports.up = function(knex) {
-    return knex.schema
-        .createTable('champions', tbl => {
-            tbl.increments(); //create id and auto increment
-            tbl.text('champion_name')
-                .notNullable();
-            tbl.boolean('champion_pic')
-            tbl.boolean('top')
-            tbl.boolean('jungle')
-            tbl.boolean('mid')
-            tbl.boolean('adc')
-            tbl.boolean('support')
-        })
+exports.up = function (knex) {
+  return knex.schema.createTable("champions", (tbl) => {
+    tbl.increments(); //create id and auto increment
+    tbl.text("champion_name").notNullable();
+    tbl.text("champion_pic");
+    tbl.boolean("top");
+    tbl.boolean("jungle");
+    tbl.boolean("mid");
+    tbl.boolean("adc");
+    tbl.boolean("support");
+  });
 };
 
-exports.down = function(knex) {
-    return knex.schema
-        .dropTableIfExists('champions')
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("champions");
 };
