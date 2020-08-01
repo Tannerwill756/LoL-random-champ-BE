cc83853e1ee91ef85bea92183760ad85992fa44a
Tanner Williams <tannerwill756@gmail.com>
2020-08-01 15:50:34 -0400

    added cors

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
 
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+});
+
 server.use("/api/champs", ChampRouter);
 
 module.exports = server;
