4a831f8177d3acdbf2e01e74ce0e36d2cb0b9e7f
Tanner Williams <tannerwill756@gmail.com>
2020-08-05 16:56:08 -0400

    fixing build fail

--- knexfile.js
+++ knexfile.js
@@ -2,24 +2,24 @@
 const connectionString = process.env.DATABASE_URL;
 
 module.exports = {
-  development: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./data/champs.db3",
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      },
-    },
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
-  },
+  // development: {
+  //   client: "sqlite3",
+  //   useNullAsDefault: true,
+  //   connection: {
+  //     filename: "./data/champs.db3",
+  //   },
+  //   pool: {
+  //     afterCreate: (conn, done) => {
+  //       conn.run("PRAGMA foreign_keys = ON", done);
+  //     },
+  //   },
+  //   migrations: {
+  //     directory: "./data/migrations",
+  //   },
+  //   seeds: {
+  //     directory: "./data/seeds",
+  //   },
+  // },
 
   production: {
     client: "pg",
