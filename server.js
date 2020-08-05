9f30c12670f8b1c4c98dd5cbf7e0ab2f711c26ed
Tanner Williams <tannerwill756@gmail.com>
2020-08-05 16:53:05 -0400

    fixed cors

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 const express = require("express");
-const cors = require("cors");
 const ChampRouter = require("./requests/request.js");
 
 const server = express();
 
-// const corsOptions = {
-//   origin: "https://lol-random-champ.vercel.app/",
-// };
-
-server.options("*", cors());
+server.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 
 server.use(express.json());
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
-
-// server.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "*");
-// });
 
 server.use("/api/champs", ChampRouter);
 
